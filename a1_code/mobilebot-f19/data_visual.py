4d34a2362adee1059b476fc02ebe2d0b764591f4
Kun <kun@gmail.com>
2019-09-15 22:55:43 -0400

    finish velocity visualization

---
+++ a1_code/mobilebot-f19/data_visual.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt 
+
+f = open("../log/data-new.txt","r")
+lines = f.readlines()
+# count = 1
+datas = []
+for line in lines:
+    # print(line)
+    if '|' in line and 'SENSORS' not in line and 'X' not in line:
+        data = [float(i) for i in line.split("|")]
+        # print(data)
+        datas.append(data[0])
+        # plt.plot(count, data[1])
+        # count += 1
+print(datas)
+plt.plot(datas)
+plt.show()
