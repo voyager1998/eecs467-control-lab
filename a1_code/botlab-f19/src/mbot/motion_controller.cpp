5099b608237d328185878e48b4cf47127de3e944
Kun <kun@gmail.com>
2019-09-22 14:49:34 -0400

    task 6 almost finished

--- a1_code/botlab-f19/src/mbot/motion_controller.cpp
+++ a1_code/botlab-f19/src/mbot/motion_controller.cpp
@@ -22,14 +22,16 @@
 
 #define STOPTIME 100
 #define PI 3.14159265358979323846
+#define DESEIREDDIST 0.5
+#define K1 0.5
+#define K2 0.1
+
 class MotionController {
 public:
     /**
     * Constructor for MotionController.
     */
     MotionController(lcm::LCM *instance) : state_(State::DRIVE),
-                                           wallfollower_k1(0.0),
-                                           wallfollower_k2(sqrt(4 * wallfollower_k1)),
                                            keep_heading(0.0f),
                                            lcmInstance(instance) {
         time_offset = 0;
@@ -119,6 +121,68 @@ public:
         return cmd;
     }
 
+    mbot_motor_command_t updateCommandLidar(void) {
+        mbot_motor_command_t cmd;
+        cmd.trans_v = 0.0f;
+        cmd.angular_v = 0.0f;
+        cmd.utime = now();
+
+        if (state_ == TURN) {
+            cmd.trans_v = 0.0f;
+            if (back_wall_dist < DESEIREDDIST + 0.1 && front_wall_dist > 2 - DESEIREDDIST - 0.1) {
+                cmd.angular_v = 0.0f;
+                state_ = DRIVE;
+            } else {
+                cmd.angular_v = -0.5f;
+                printf("Turning, angular velocity: %f\n", cmd.angular_v);
+            }
+            // float diff = 0.0;
+            // if (cur_pos.theta - pose_target.theta < -M_PI) {
+            //     diff = pose_target.theta - (cur_pos.theta + 2 * M_PI);
+            // } else if (cur_pos.theta - pose_target.theta > M_PI) {
+            //     diff = pose_target.theta + 2 * M_PI - cur_pos.theta;
+            // } else {
+            //     diff = pose_target.theta - cur_pos.theta;
+            // }
+
+            // if (fabs(diff) <= 0.1) {
+            //     keep_heading = pose_target.theta;
+            //     state_ = DRIVE;
+            //     targets_.pop_back();
+            // } else {
+            //     if (diff > 0) {
+            //         cmd.angular_v = -1 * (diff + 0.1);
+            //     } else {
+            //         cmd.angular_v = -1 * (diff - 0.1);
+            //     }
+            // }
+        } else if (state_ == DRIVE) {
+            if (front_wall_dist < DESEIREDDIST + 0.1) {
+                cmd.trans_v = 0.0f;
+                cmd.angular_v = 0.0f;
+                state_ = TURN;
+            } else {
+                // float debug_trans_v = std::min(1.2 * diff + 0.05, 0.1);  //0.1f;
+                // float ang_diff = 0.0;
+                // float dir = atan2((pose_target.y - cur_pos.y), (pose_target.x - cur_pos.x));
+                // if (cur_pos.theta - dir < -M_PI) {
+                //     ang_diff = dir - (cur_pos.theta + 2 * M_PI);
+                // } else if (cur_pos.theta - dir > M_PI) {
+                //     ang_diff = dir + 2 * M_PI - cur_pos.theta;
+                // } else {
+                //     ang_diff = dir - cur_pos.theta;
+                // }
+                cmd.trans_v = 0.2f;
+                cmd.angular_v = -(-K1 * theta_rightwall - K2 / cmd.trans_v * (right_wall_dist - DESEIREDDIST));
+                printf("angular velocity: %f\n", cmd.angular_v);
+            }
+        } else {
+            std::cerr << "ERROR: MotionController: Entered unknown state: " << state_ << '\n';
+        }
+
+        return cmd;
+    }
+
     bool timesync_initialized() { return timesync_initialized_; }
 
     void handleTimesync(const lcm::ReceiveBuffer *buf, const std::string &channel, const timestamp_t *timesync) {
@@ -164,7 +228,7 @@ public:
         cur_state.right_velocity = state->right_velocity;
     }
 
-    void handleLIDAR(const lcm::ReceiveBuffer *buf, const std::string &channel, const lidar_t *newLidar) {
+    void handleLIDAR1(const lcm::ReceiveBuffer *buf, const std::string &channel, const lidar_t *newLidar) {
         int count = newLidar->num_ranges;
         std::vector<int> corners;
         int compare_num = 20;
@@ -246,7 +310,48 @@ public:
             }
             printf("world frame pose: %f, %f\n", cur_wf_pos.x, cur_wf_pos.y);
         }
-    }
+    }  //find 4 corners and then fit 4 walls
+
+    void handleLIDAR2(const lcm::ReceiveBuffer *buf, const std::string &channel, const lidar_t *newLidar) {
+        int count = newLidar->num_ranges;
+        std::vector<float> right_wall;  //(75~105)
+        std::vector<float> front_wall;  //(0~15) U (345~360)
+        std::vector<float> back_wall;   //(165~195)
+
+        int index_right_dist = 0;
+        float dist_to_right = 5.0f;
+        for (int i = 0; i < count; i++) {
+            if (newLidar->thetas[i] > 75.0 / 180.0 * M_PI && newLidar->thetas[i] < 105.0 / 180.0 * M_PI) {
+                if (newLidar->ranges[i] > 0.2) {
+                    right_wall.push_back(newLidar->ranges[i]);
+                    if (newLidar->ranges[i] < dist_to_right) {
+                        dist_to_right = newLidar->ranges[i];
+                        index_right_dist = i;
+                    }
+                }
+            } else if ((newLidar->thetas[i] > 0 && newLidar->thetas[i] < 15.0 / 180.0 * M_PI) || (newLidar->thetas[i] > 345.0 / 180.0 * M_PI && newLidar->thetas[i] < 2 * M_PI)) {
+                if (newLidar->ranges[i] > 0.2) {
+                    front_wall.push_back(newLidar->ranges[i]);
+                }
+            } else if (newLidar->thetas[i] > 165.0 / 180.0 * M_PI && newLidar->thetas[i] < 195.0 / 180.0 * M_PI) {
+                if (newLidar->ranges[i] > 0.2) {
+                    back_wall.push_back(newLidar->ranges[i]);
+                }
+            }
+        }
+        // float dist_to_right = *std::min_element(right_wall.begin(), right_wall.end());
+        float dist_to_front = *std::min_element(front_wall.begin(), front_wall.end());
+        float dist_to_back = *std::min_element(back_wall.begin(), back_wall.end());
+
+        printf("distance to right wall: %f\n", dist_to_right);
+        printf("distance to front wall: %f\n", dist_to_front);
+
+        right_wall_dist = dist_to_right;
+        front_wall_dist = dist_to_front;
+        back_wall_dist = dist_to_back;
+        theta_rightwall = newLidar->thetas[index_right_dist] - M_PI_2;
+        // lcmInstance->publish(LIDAR_POSE_CHANNEL, &cur_wf_pos);
+    }  //only concentrate on small ranges in the forward direction and right direction
 
     /**
      * ZHIHAO RUAN:
@@ -315,9 +420,11 @@ private:
     pose_xyt_t cur_pos;
     pose_xyt_t cur_wf_pos;
     curr_state_t cur_state;
-    float wallfollower_k1;
-    float wallfollower_k2;
     float keep_heading = 0.0f;  // The heading to keep when driving straight
+    float right_wall_dist;
+    float front_wall_dist;
+    float back_wall_dist;
+    float theta_rightwall;  // counter-clock wise, when perpendicular to right wall: 0
 
     int64_t time_offset;
     bool timesync_initialized_;
@@ -342,14 +449,14 @@ int main(int argc, char **argv) {
     // For instance, instantaneous translational and rotational velocity of the robot, which is necessary
     // for your feedback controller.
     lcmInstance.subscribe(MBOT_STATE_CHANNEL, &MotionController::handleState, &controller);
-    lcmInstance.subscribe(LIDAR_CHANNEL, &MotionController::handleLIDAR, &controller);
+    lcmInstance.subscribe(LIDAR_CHANNEL, &MotionController::handleLIDAR2, &controller);
 
     signal(SIGINT, exit);
 
     while (true) {
         lcmInstance.handleTimeout(20);  // update at 50Hz minimum
         if (controller.timesync_initialized()) {
-            mbot_motor_command_t cmd = controller.updateCommand();
+            mbot_motor_command_t cmd = controller.updateCommandLidar();
             lcmInstance.publish(MBOT_MOTOR_COMMAND_CHANNEL, &cmd);
         }
     }
